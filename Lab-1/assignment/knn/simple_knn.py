c1dc90a53e3b6f58e44d7394bb3d766748709ade
Talha Ibne Mahmud <71333752+talhaibnmahmud@users.noreply.github.com>
2022-04-04 02:29:08 +0600

    Fixed styling issues and Added #type: ignore

--- Lab-1/assignment/knn/simple_knn.py
+++ Lab-1/assignment/knn/simple_knn.py
@@ -1,3 +1,4 @@
+""" A Simple KNN Implementation"""
 from collections import Counter
 
 import numpy as np
@@ -5,30 +6,33 @@ import numpy.typing as npt
 
 
 def euclidean_distance(x: npt.NDArray[np.float64], y: npt.NDArray[np.float64]) -> np.float64:
+    """Calculate the Euclidean distance between two vectors.
     """
-    Calculate the Euclidean distance between two vectors.
-    """
-    return np.sqrt(np.sum((x - y) ** 2))
+    return np.sqrt(np.sum((x - y) ** 2), dtype=np.float64)  # type: ignore
 
 
 class SimpleKNN:
+    """Simple KNN Class"""
+
     def __init__(self, n_neighbors: int):
         self.k = n_neighbors
-    
+
     def fit(self, x: npt.NDArray[np.float64], y: npt.NDArray[np.int8]) -> None:
+        """Train Model"""
         self.x_train = x
         self.y_train = y
 
     def predict(self, test: npt.NDArray[np.float64]) -> npt.NDArray[np.int8]:
+        """ Predict Result"""
         predicted_labels = [self._calculate(x) for x in test]
-        return np.array(predicted_labels)
-    
+        return np.array(predicted_labels)   # type: ignore
+
     def _calculate(self, x: npt.NDArray[np.float64]) -> np.int8:
         # calculate the distance between x and each point in x_train
         distances = [euclidean_distance(x, sample) for sample in self.x_train]
 
         # find the k nearest points
-        k_nearest_idx: npt.NDArray[np.float64] = np.argsort(distances)[:self.k]
+        k_nearest_idx: npt.NDArray[np.float64] = np.argsort(distances)[:self.k] # type: ignore
         k_nearest_labels = [self.y_train[i] for i in k_nearest_idx]
 
         # return the most common class label
